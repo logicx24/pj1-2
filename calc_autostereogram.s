e4b52f6e34b5ca81b591ef36312642ec30913151
gintrator <g.intrator@gmail.com>
2014-09-28 22:51:13 -0700

    basic outline part a

--- calc_autostereogram.s
+++ calc_autostereogram.s
@@ -12,63 +12,68 @@ calc_autostereogram:
 
         # Allocate 5 spaces for $s0-$s5
         # (add more if necessary)
-        addiu $sp $sp -20
+        addiu $sp $sp -40
         sw $s0 0($sp)
         sw $s1 4($sp)
         sw $s2 8($sp)
         sw $s3 12($sp)
         sw $s4 16($sp)
+        sw $s5 20($sp)
+        sw $s6 24($sp)
+        sw $s7 28($sp)
 
         # autostereogram
-        lw $s0 20($sp)
+        lw $s0 40($sp)
         # depth_map
-        lw $s1 24($sp)
+        lw $s1 44($sp)
         # width
-        lw $s2 28($sp)
+        lw $s2 48($sp)
         # height
-        lw $s3 32($sp)
+        lw $s3 52($sp)
         # strip_size
-        lw $s4 36($sp)
+        lw $s4 56($sp)
 
         # YOUR CODE HERE #
-
-        addiu $t0, $zero, 0 #int i = 0
-        addiu $t1, $zero, 0 #int j = 0
+		
+		
+        addiu $s5, $zero, 0 #int i = 0
+        addiu $s6, $zero, 0 #int j = 0
 
         OUTERLOOP:
                 INNERLOOP:
-                        blt $t0, $s4, TRUE
+                        blt $s5, $s4, TRUE
                         j FALSE
                         TRUE:
-                                mul $t2, $t1, $s2 #j*width
-                                add $t2, $t2, $t0
-                                addu $t3, $t2, $s0 #calculated array index of autosterogram
-                                addu $t4, $zero, $zero
+                        		move $s7, $ra
                                 jal lfsr_random
+                                move $ra, $s7
                                 move $t4, $v0
+                                mul $t2, $s6, $s2 #j*width
+                                add $t2, $t2, $s5 # add i to j * width
+                                addu $t3, $t2, $s0 #calculated array index of autosterogram
                                 and $t4, $t4, 0xFF
                                 sb $t4, 0($t3)
-                        j CONDITION
+                        		j CONDITION
                         FALSE:
-                                mul $t2, $t1, $s2 #j*width
-                                add $t2, $t2, $t0
+                                mul $t2, $s6, $s2 #j*width
+                                add $t2, $t2, $s5
                                 addu $t3, $t2, $s0 #calculated array index of autosterogram  
                                 addu $t4, $t2, $s1 #calculated array index of depth_map  
-                                lw $t5, 0($t4)
-                                add $t5, $t5, $t0
+                                lb $t5, 0($t4)
+                                add $t5, $t5, $s5
                                 sub $t5, $t5, $s4
 
-                                mul $t2, $t1, $s2 #j*width
+                                mul $t2, $s6, $s2 #j*width
                                 add $t2, $t2, $t5
-                                addu $t6, $t2, $s0 #calculated array index of autosterogram
-                                lw $t7, 0($t6)
+                                addu $s5, $t2, $s0 #calculated array index of autosterogram
+                                lb $s6, 0($s5)
 
-                                sb $t7, 0($t3)
+                                sb $s6, 0($t3)
                         CONDITION:
-                                addi $t1, $t1, 1
-                                blt $t1, $s3, INNERLOOP
-                                addi $t0, $t0, 1
-                                blt $t0, $s2, OUTERLOOP
+                                addi $s6, $s6, 1
+                                blt $s6, $s3, INNERLOOP
+                        addi $s5, $s5, 1
+                        blt $s5, $s2, OUTERLOOP
 
 
 
@@ -84,5 +89,8 @@ calc_autostereogram:
         lw $s2 8($sp)
         lw $s3 12($sp)
         lw $s4 16($sp)
-        addiu $sp $sp 20
+        lw $s5 20($sp)
+        lw $s6 24($sp)
+        lw $s7 28($sp)
+        addiu $sp $sp 40
         jr $ra
